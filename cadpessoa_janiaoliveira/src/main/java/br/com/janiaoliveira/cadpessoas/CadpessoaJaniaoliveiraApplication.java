085bccb6b281d598510e8b38dca6d9430d97cfb1
Janiadev <81199771+Janiadev@users.noreply.github.com>
2021-04-09 20:26:10 -0300

    Iniciado o projeto

---
+++ cadpessoa_janiaoliveira/src/main/java/br/com/janiaoliveira/cadpessoas/CadpessoaJaniaoliveiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.janiaoliveira.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoaJaniaoliveiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoaJaniaoliveiraApplication.class, args);
+	}
+
+}
---
+++ cadpessoa_janiaoliveira/src/test/java/br/com/janiaoliveira/cadpessoas/CadpessoaJaniaoliveiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.janiaoliveira.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoaJaniaoliveiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
