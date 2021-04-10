8784644050ad17449251685375fa8ecca5fb141d
Janiadev <81199771+Janiadev@users.noreply.github.com>
2021-04-09 21:56:32 -0300

    iniciado os controles e copiando o html

--- cadpessoa_janiaoliveira/src/main/java/br/com/janiaoliveira/cadpessoas/controllers/PessoaController.java
+++ cadpessoa_janiaoliveira/src/main/java/br/com/janiaoliveira/cadpessoas/controllers/PessoaController.java
@@ -1,10 +1,19 @@
 package br.com.janiaoliveira.cadpessoas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.janiaoliveira.cadpessoas.repository.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
 
+	@Autowired
+	PessoaRepository pessoaRepo;
+	
+	public String index() {
+		return "index.html";
+	}
 }
