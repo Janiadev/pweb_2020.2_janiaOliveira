fc1f02b9292363de857a609b7e21cab875dec9b9
Janiadev <81199771+Janiadev@users.noreply.github.com>
2021-04-09 22:51:07 -0300

    inserçao do codigo secreto

--- cadpessoa_janiaoliveira/src/main/java/br/com/janiaoliveira/cadpessoas/controllers/PessoaController.java
+++ cadpessoa_janiaoliveira/src/main/java/br/com/janiaoliveira/cadpessoas/controllers/PessoaController.java
@@ -1,9 +1,15 @@
 package br.com.janiaoliveira.cadpessoas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.janiaoliveira.cadpessoas.model.Pessoa;
 import br.com.janiaoliveira.cadpessoas.repository.PessoaRepository;
 
 @Controller
@@ -15,5 +21,13 @@ public class PessoaController {
 	
 	public String index() {
 		return "index.html";
+	}
+	
+	@GetMapping("/listarPessoas")
+	public ModelAndView listarPessoas() {
+		List <Pessoa> lista = pessoaRepo.findAll();
+		ModelAndView mav = new ModelAndView("listarPessoas");
+		mav.addObject("pessoas", lista);
+		return mav;
 	}
 }
