686557a69d4cce83619cdf9720e660bcf1958a77
Janiadev <81199771+Janiadev@users.noreply.github.com>
2021-04-26 22:50:35 -0300

    completando controller

--- agropopshop_janiaoliveira/src/main/java/br/com/janiaoliveira/agropopshop/agropopshop_janiaoliveira/controller/ClienteController.java
+++ agropopshop_janiaoliveira/src/main/java/br/com/janiaoliveira/agropopshop/agropopshop_janiaoliveira/controller/ClienteController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,4 +33,27 @@ public class ClienteController {
 		mav.addObject("cliente", lista);
 		return mav;
 	}
+	
+	@GetMapping("/adicionarCliente")
+	public ModelAndView formAdicionarCliente() {
+		ModelAndView modelView = new ModelAndView("adicionarClientes");
+		modelView.addObject(new Cliente());
+		return modelView;
+	}
+	
+	@PostMapping("/adicionarClientes")
+	public ModelAndView adicionarClientes(Cliente cliente) {
+		clienteRepo.save(cliente);
+		return new ModelAndView("redirect:/listarClientes");
+	}
+	
+	@GetMapping("/editarClientes/{id}")
+	public ModelAndView formEditarCliente(@PathVariable("id") long id) {
+		Cliente cliente = clienteRepo.findById(id).orElseThrow(
+				()-> new IllegalAccessError("Não existe"));
+		ModelAndView modelAndView = new ModelAndView("editarClientes");
+		modelAndView.addObject(cliente);
+		return modelAndView;
+	}
+	
 }
