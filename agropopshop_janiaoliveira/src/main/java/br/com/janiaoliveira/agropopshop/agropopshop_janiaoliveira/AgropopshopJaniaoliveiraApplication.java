49bb3b84fb7897bde9a2828575222bb8cc047d79
Janiadev <81199771+Janiadev@users.noreply.github.com>
2021-04-26 21:59:37 -0300

    iniciando

---
+++ agropopshop_janiaoliveira/src/main/java/br/com/janiaoliveira/agropopshop/agropopshop_janiaoliveira/AgropopshopJaniaoliveiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.janiaoliveira.agropopshop.agropopshop_janiaoliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgropopshopJaniaoliveiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgropopshopJaniaoliveiraApplication.class, args);
+	}
+
+}
---
+++ agropopshop_janiaoliveira/src/test/java/br/com/janiaoliveira/agropopshop/agropopshop_janiaoliveira/AgropopshopJaniaoliveiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.janiaoliveira.agropopshop.agropopshop_janiaoliveira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgropopshopJaniaoliveiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
